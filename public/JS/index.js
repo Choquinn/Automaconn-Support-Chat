ad2d3c3378c0f2e1904ca305a85ec2fbe3a64c26
Choquinn <voltzchoquinn@gmail.com>
2025-10-23 10:19:51 -0300

    Desconectar whatsapp completo

--- public/JS/index.js
+++ public/JS/index.js
@@ -9,45 +9,22 @@ function changeTab(tab) {
   fetchConversations();
 }
 
-async function waitForConnection() {
-  const qrContainer = document.getElementById("qr");
+async function checkConnection() {
+  const statusRes = await fetch("/status");
+  const { status } = await statusRes.json();
 
-  while (true) {
-    try {
-      // 1️⃣ Buscar status do bot
-      const statusRes = await fetch("/status");
-      const { status } = await statusRes.json();
-
-      if (status === "conectado") {
-        // Redireciona assim que estiver conectado
-        window.location.href = "index.html";
-        return;
-      }
-
-      // 2️⃣ Buscar QR code
-      const qrRes = await fetch("/qr");
-      if (qrRes.ok) {
-        const { qr } = await qrRes.json();
-        const qrImage = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qr)}`;
-        qrContainer.innerHTML = `<img src="${qrImage}" alt="QR Code">`;
-      } else {
-        qrContainer.innerHTML = "<p>QR ainda não disponível...</p>";
-      }
-
-    } catch (err) {
-      console.error("Erro ao buscar status ou QR:", err);
-    }
-
-    // 3️⃣ Espera 2 segundos antes de checar novamente
-    await new Promise(resolve => setTimeout(resolve, 2000));
+  if (status === "conectado") {
+    return; // para de checar
+  }else {
+    window.location.href = "/connect.html";
   }
 }
 
-// Inicia a função
-// waitForConnection();
+checkConnection();
  
 async function quitSession(){
     await fetch("/exit");
+    checkConnection();
 }
 
 // ===== Buscar conversas =====
